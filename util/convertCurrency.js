898344dbb0726d3b1a4dbf5eebaf045a49266681
hewerton <hewerton80@gmail.com>
2023-04-27 19:50:47 -0300

    add verification aws lambda

--- util/convertCurrency.js
+++ util/convertCurrency.js
@@ -1,16 +1,27 @@
-const puppeteer = require('puppeteer')
-const puppeteerCore = require('puppeteer-core')
+let chromium = {}
+let puppeteer
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chromium = require('chrome-aws-lambda')
+  puppeteer = require('puppeteer-core')
+} else {
+  puppeteer = require('puppeteer')
+}
 
 exports.convertCurrency = async (fromCurrencyCode, toCurrencyCode) => {
-  const browser = await puppeteerCore.launch({
-    args: puppeteerCore.args,
-    defaultViewport: puppeteerCore.defaultViewport,
-    executablePath: puppeteer.executablePath(),
-    headless: 'new',
-    ignoreHTTPSErrors: true,
-    timeout: 90000, // 90 seconds
-  })
-  console.log('executablePath: ', puppeteer.executablePath())
+  let options = {}
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: 'new',
+      ignoreHTTPSErrors: true,
+    }
+  } else {
+  }
+
+  const browser = await puppeteer.launch(options)
   const page = await browser.newPage()
 
   await page.goto(
